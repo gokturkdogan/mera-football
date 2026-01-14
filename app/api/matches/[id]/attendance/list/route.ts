39beaba08a673955a7759c53c1b083533df908e5
Göktürk DOĞAN <gokturk.dogan@hotmail.com>
2026-01-14 15:05:57 +0300

    Redesign

--- app/api/matches/[id]/attendance/list/route.ts
+++ app/api/matches/[id]/attendance/list/route.ts
@@ -26,7 +26,6 @@ export async function GET(
       )
     }
 
-    // Check if match exists and user is owner
     const match = await prisma.match.findUnique({
       where: { id: params.id },
       include: {
